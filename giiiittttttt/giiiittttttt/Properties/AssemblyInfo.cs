d7bc28e1467b8b53d2947bc7080c907ab3ad1741
DELL <DELL@DELL-DATOR>
2016-09-07 10:54:40 +0200

    hej

--- giiiittttttt/giiiittttttt/Properties/AssemblyInfo.cs
+++ giiiittttttt/giiiittttttt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication3")]
+[assembly: AssemblyTitle("giiiittttttt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication3")]
+[assembly: AssemblyProduct("giiiittttttt")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21239e46-f065-4ce4-8dee-1ed74ed44adf")]
+[assembly: Guid("5b87e511-d60f-44c7-b32d-593d514be8a8")]
 
 // Version information for an assembly consists of the following four values:
 //
